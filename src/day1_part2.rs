c236c792cbbe790a47944eb989d920967e8ab1e8
Julian Krannich <julian.krannich@check24.de>
2025-12-01 23:08:16 +0100

    day 1 part 2 solution

--- src/day1_part2.rs
+++ src/day1_part2.rs
@@ -1,3 +1,35 @@
 pub fn run() {
-    
+    //new twist: we now want to count every time we cross 0 too
+    //how to do it?
+    //loop distance amount of times
+    //each iteration = one move to left/right
+    //each move -> currentdial++
+    //check if current dial == 0 -> if yes counter++
+    let input = include_str!("../inputs/day1input.txt");
+
+    let mut starting_dial: i32 = 50;
+    let mut counter = 0;
+
+    for line in input.lines() {
+        let (direction, number) = line.split_at(1);
+        let distance: i32 = number.parse().unwrap();
+
+        for _ in 0..distance {
+            match direction {
+                "L" => starting_dial -= 1,
+                "R" => starting_dial += 1,
+                _ => unreachable!(),
+            };
+
+            starting_dial = starting_dial.rem_euclid(100);
+
+            if starting_dial == 0 {
+                counter += 1;
+            }
+
+            println!("{counter}");
+        }
+
+        println!("{}", counter)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let day = args.get(1).map(String::as_str).unwrap_or("1");
 
     match day {
-        "1" => day1_part1::run(),
+        "1.1" => day1_part1::run(),
+        "1.2" => day1_part2::run(),
         "2" => day2::run(),
         _ => println!("Unknown day: {day}"),
     }
