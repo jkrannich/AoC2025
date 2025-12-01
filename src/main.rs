96990209cb60a9cafa8a0644647311e0120ba390
Julian Krannich <julian.krannich@check24.de>
2025-12-01 22:21:48 +0100

    skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod day1;
+mod day2;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let day = args.get(1).map(String::as_str).unwrap_or("1");
+
+    match day {
+        "1" => day1::run(),
+        "2" => day2::run(),
+        _ => println!("Unknown day: {day}"),
+    }
 }
