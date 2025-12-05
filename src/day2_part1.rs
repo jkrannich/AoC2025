0f014e67ec316d962300d8f6ccc2927554c29fb8
Julian Krannich <julian.krannich@check24.de>
2025-12-05 14:00:06 +0100

    day 2 part 1: loop through numbers in ranges and convert to string, check if string is dividable by 2 and if yes -> check if first half equal to last half

--- src/day2_part1.rs
+++ src/day2_part1.rs
@@ -15,4 +15,36 @@ pub fn run() {
         .collect();
 
     println!("{ranges:#?}");
+
+    //intialize result int = 0
+    //for each (start, end) in ranges
+    //for number in start..=end
+    //convert number -> string
+    //if string.length modulo 2 = 0 -> possible invalid id
+    //for each entry in possible numbers list/vec
+    //split number in half
+    //if first half of digits == second half of digits -> invalid input
+    //add to result int
+    //return result int
+
+    let mut result = 0;
+
+    for (start, end) in &ranges {
+        for number in *start..=*end {
+            let s = number.to_string();
+
+            if s.len() % 2 == 1 {
+                continue;
+            }
+
+            let half = s.len()/2;
+            let first_half = &s[..half];
+            let second_half = &s[half..];
+
+            if first_half == second_half {
+                result += number;
+            }
+            println!("{}", result)
+        }
+    }
 }
